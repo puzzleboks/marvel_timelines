1d78b8570589fc5cea0f4b6204ce88460b62771f
lon phelps <puzzleboks@gmail.com>
2015-09-14 20:51:59 -0400

    Added input form and ajax request

--- marvel_timelines.js
+++ marvel_timelines.js
@@ -1,20 +1,46 @@
+
+
 if (Meteor.isClient) {
-  Template.body.events({
-    "submit .new-task": function (event) {
+  myImgPath = "";
+  //console.log(test)
+  Template.heroImg.events({
+    "submit .new-hero": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
-
+      // myImgPath = "";
       // Get value from form element
-      var text = event.target.text.value;
-
-      // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
+      var privKey = "";
+      var pubKey = "e687d607d622b25c31d6ae38f2f42597";
+      var name = event.target.text.value;
+      var ts = parseInt(Date.now()/1000, 10);
+      var preHash = ts + privKey + pubKey;
+      var hash = CryptoJS.MD5(preHash).toString();
+      var url = "http://gateway.marvel.com:80/v1/public/characters?name=" + name + "&ts=" + ts + "&apikey=e687d607d622b25c31d6ae38f2f42597&hash=" + hash
+      // console.log(url)
+      $.ajax({
+        // the URL endpoint for the JSON object
+        url: url,
+        // type of request
+        type: "get",
+        // datatype xml or json
+        dataType: "json"
+        // promise that executes on successful ajax call
+      }).done(function(response){
+        myImgPath = response.data.results[0].thumbnail.path + "/portrait_incredible" + ".jpg"
+        console.log(response)
+        console.log(myImgPath)
+        // console.log(response.data.results[0].id)
+      })
+      // console.log(myImgPath)
       // Clear form
       event.target.text.value = "";
+    }
+  });
+
+  Template.heroImg.helpers({
+    avatarPath: function () {
+      //console.log(myImgPath)
+      return myImgPath;
     }
   });
 }
